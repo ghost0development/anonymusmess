2db9ab49a4e8c576c95ff41830035ecdec25efe3
Dominic <dominic@localhost.localdomain>
2026-06-15 20:00:28 +0200

    deploy: fix SPA build

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({ strict: false }),
+		adapter: adapter({ strict: false, fallback: 'index.html' }),
 		paths: {}
 	}
 };
